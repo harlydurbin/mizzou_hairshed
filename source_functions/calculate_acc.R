da49597e66605e554e80f4b0ffe14a7aefc83646
harlydurbin <hjdzpd@mail.missouri.edu>
2020-10-26 15:03:42 -0500

    Work on calculating pedigree-based inbreeding

--- source_functions/calculate_acc.R
+++ source_functions/calculate_acc.R
@@ -2,14 +2,14 @@
 # * $PEV = d_i\sigma^2_e$
 #   * $r^2 = 1-(\frac{PEV}{\sigma^2_u})$
 
-calculate_acc <- function(u, se, diagonal, option = "reliability") {
+calculate_acc <- function(u, se, f, option = "reliability") {
   PEV <- (se ^ 2)
   
   acc <- if (option == "reliability") {
-    1 - (PEV / (diagonal*u))
+    1 - (PEV / ((1+f)*u))
   } else if(option == "bif"){
     
-    1 - sqrt((PEV / (diagonal*u)))
+    1 - sqrt((PEV / ((1+f)*u)))
     
   }
   
