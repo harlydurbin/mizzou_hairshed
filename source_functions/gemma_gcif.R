335013439bdcc36531cee9e015181a810ee1fe22
harlydurbin <hjdzpd@mail.missouri.edu>
2021-01-26 11:51:55 -0600

    Testing parent average included vs. removed results, updated genomic control inflation factor function

--- source_functions/gemma_gcif.R
+++ source_functions/gemma_gcif.R
@@ -1,11 +1,18 @@
 
 
 gcif <- 
-  function(df, adjust_p = FALSE){
-    gci <- median(qchisq(df$p_wald, 1, lower.tail=FALSE))/qchisq(0.5, 1)
+  function(df, p_col = p_wald, adjust_p = FALSE){
+    
+    p_col <- rlang::enquo(p_col)
+    
+    df <- 
+      df %>% 
+      rename(p = !!p_col)
+    
+    gci <- median(qchisq(df$p, 1, lower.tail=FALSE))/qchisq(0.5, 1)
     
     if(adjust_p == TRUE){
-      new_p <- pchisq(qchisq(df$p_wald, 1, lower.tail=FALSE)/gci, df = 1, lower.tail = FALSE)
+      new_p <- pchisq(qchisq(df$p, 1, lower.tail=FALSE)/gci, df = 1, lower.tail = FALSE)
       return(new_p)
     } else {
       return(gci)
