98c51ccb2e659dd389fc4458f8ae3deaf2641bc0
harlydurbin <hjdzpd@mail.missouri.edu>
2021-01-30 12:58:32 -0600

    Better notes in estimate_bias.Rmd, one last attempt at GxE GWAS (pre-adjust for CG including farm but excluding score group)

--- source_functions/ggqq.R
+++ source_functions/ggqq.R
@@ -2,18 +2,23 @@
 
 ggqq <- function(pvector){
   pvector = pvector[!is.na(pvector) & !is.nan(pvector) & !is.null(pvector) & is.finite(pvector) & pvector<1 & pvector>0]
-  pdf = data.frame(observed = -log10(sort(pvector, decreasing = FALSE)), expected = -log10(ppoints(length(pvector))))
+  pdf = data.frame(observed = -log10(sort(pvector, decreasing = FALSE)),
+                   expected = -log10(ppoints(length(pvector))))
   
-  upper_limit = max(max(pdf$observed), max(pdf$expected))
+  upper_limit = max(max(pdf$observed),
+                    max(pdf$expected))
   
-  qqplotted = ggplot(pdf, aes(expected, observed)) +
-    geom_point() +
-    geom_abline(intercept = 0,
-                slope = 1,
-                colour = "red") +
-    scale_x_continuous(limits = c(0, upper_limit)) +
-    scale_y_continuous(limits = c(0, upper_limit)) +
-    labs(x = expression(paste("Observed ", -log10, "(", italic('p'), ")")),
-         y = expression(paste("Expected ", -log10, "(", italic('p'), ")")))
+  qqplotted <-
+    ggplot2::ggplot(pdf,
+                    ggplot2::aes(expected, observed)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_abline(intercept = 0,
+                         slope = 1,
+                         colour = "red") +
+    ggplot2::scale_x_continuous(limits = c(0, upper_limit)) +
+    ggplot2::scale_y_continuous(limits = c(0, upper_limit)) +
+    ggplot2::labs(x = expression(paste("Observed ", -log10, "(", italic('p'), ")")),
+                  y = expression(paste("Expected ", -log10, "(", italic('p'), ")")))
+  
   return(qqplotted)
 }
